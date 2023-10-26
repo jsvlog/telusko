bc63f507aa44fc3fa2e4a85988e87b540d3b1797
js <lizardojohnsonc@gmail.com>
2023-10-26 11:06:40 +0800

    minor

--- wow/settings.py
+++ wow/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# STATICFILES_DIRS and STATIC_ROOT - to recognize the css and js at static folder, it will create assets directory also
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
