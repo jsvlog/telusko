3fd5a0525a1622f20203343145bc426d6c0e4820
jsvlog <lizardojohnsonc@gmail.com>
2023-10-28 11:11:03 +0800

    aja

--- travello/views.py
+++ travello/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import form
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html',{'name':'js'})
+    addInfo = form()
+    addInfo.addInfo = "ADDITIONAL INFO"
+    addInfo.basicInfo = "BASIC INFO"
+    return render(request, 'index.html',{'right': addInfo})
